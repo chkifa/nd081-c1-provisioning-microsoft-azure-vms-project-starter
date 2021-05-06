9aaaaa0da69af789c7b324f2ca10d8b93263f70c
chkifa <chkifak@gmail.com>
2021-05-06 13:51:36 +0000

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,9 +11,10 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
-wsgi_app = app.wsgi_app
+logger = logging.getLogger(__name__)
+#wsgi_app = app.wsgi_app
 app.logger.setLevel(logging.WARNING)
-app.logger.info('%s logged in successfully', user.username)
+#app.logger.info('%s logged in successfully', user.username)
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.WARNING)
 app.logger.addHandler(streamHandler)
